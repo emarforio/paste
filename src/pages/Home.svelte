d894130a7c2853b379c84a5c98ca05d7a4e800df
Erik Marforio <emarforio@gmail.com>
2022-07-11 17:11:02 +0000

    Test editor and devenvironment

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,3 +1,15 @@
+<script>
+    let textContent = "";
+</script>
+
 <main class="h-full">
-    <textarea class="bg-slate-800 w-full h-full placeholder:text-slate-50" placeholder="Start typing.."></textarea>
+    <textarea
+        class="bg-slate-800 w-1 h-full text-slate-200 placeholder:text-slate-50"
+        placeholder="123"
+    ></textarea>
+    <textarea
+        class="bg-slate-800 w-9 h-full text-slate-50 placeholder:text-slate-50"
+        placeholder="Start typing.."
+        bind:value={textContent}
+    ></textarea>
 </main>
