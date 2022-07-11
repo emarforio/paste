db0cc66f4d910d9747b9965872d19a138c3f984f
Erik Marforio <emarforio@gmail.com>
2022-07-11 09:52:10 +0000

    Project setup

---
+++ src/pages/Home.svelte
@@ -0,0 +1,3 @@
+<main class="h-full">
+    <textarea class="bg-slate-800 w-full h-full placeholder:text-slate-50" placeholder="Start typing.."></textarea>
+</main>
---
+++ src/pages/View.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let id
+</script>
+
+
+<p>This is the <strong>View</strong> page, press the button to navigate to the home page</p>
+
+<p>The message passed is <strong>{id}</strong></p>
+
+<a href="/">Link to: /</a>
